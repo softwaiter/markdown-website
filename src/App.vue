e478600fafaf463e792b17a1f15f5d004657e4b8
softwaiter <softwaiter@126.com>
2021-11-03 09:29:19 +0800

    :construction:测试Github pages

--- src/App.vue
+++ src/App.vue
@@ -94,7 +94,7 @@ export default {
   },
   methods: {
     loadConfig(callback) {
-      axios.get("/static/config.json").then(res => {
+      axios.get("/markdown-website/static/config.json").then(res => {
         document.title = res.data.title;
         this.sidebarWidth = res.data.topicWidth;
         this.expandMenuLevel = res.data.openLevel;
